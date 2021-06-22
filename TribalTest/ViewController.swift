c57164ba95de3bb3f781657752e0ce439c8fe404
Uziel Sabalza <uzbrawler@gmail.com>
2021-06-22 11:02:46 -0400

    Initial Commit

---
+++ TribalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TribalTest
+//
+//  Created by wams macbook ios on 6/22/21.
+//  Copyright © 2021 wams macbook ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
